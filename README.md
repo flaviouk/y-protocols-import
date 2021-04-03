29f5584d23d8324441d890c4b533856123dec1e8
Flavio Carvalho <hey@flavio.uk>
2021-04-03 18:11:55 +0100

    Add example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # y-protocols-import
+
+Try running yarn dev or yarn build and then yarn start
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as authProtocol from 'y-protocols/auth'
+import * as awarenessProtocol from 'y-protocols/awareness'
+import * as syncProtocol from 'y-protocols/sync'
+
+console.log({
+    authProtocol,
+    awarenessProtocol,
+    syncProtocol,
+})
